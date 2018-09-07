e98ec02a783b2a44f03f668978676f6a627709a4
MacSuxx <lutzschulmail@web.de>
2018-09-07 13:19:01 +0200

    ...

--- model/src/main/java/Adress.java
+++ model/src/main/java/Adress.java
@@ -1,2 +1,55 @@
-public class Adress {
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name="ADRESS")
+public class Adress implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+
+    @Column("PLZ")
+    @Basic(optional = false)
+    String plz;
+
+    @Column("PLZ")
+    @Basic(optional = false)
+    String Street;
+
+    @Column("PLZ")
+    @Basic(optional = false)
+    String city;
+
+    public String getPlz() {
+        return plz;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getStreet() {
+        return Street;
+    }
+
+    public void setStreet(String street) {
+        Street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setPlz(String plz) {
+        this.plz = plz;
+    }
 }
