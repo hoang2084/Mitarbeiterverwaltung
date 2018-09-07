4e503f5348d8c7ebb52dd785a3d535c611130638
HNG <hng@opitz-consulting.com>
2018-09-07 10:01:41 +0200

    initial commit

---
+++ src/main/java/de/fantavier/j2ee/App.java
@@ -0,0 +1,13 @@
+package de.fantavier.j2ee;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
